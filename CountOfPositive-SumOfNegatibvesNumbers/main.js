b652ee185cfe9aae970adaba109860801a04bd86
Hatem011 <fatenkhalifa928@gmail.com>
2022-09-12 21:45:16 +0200

    CountOfPositive-SumOfNegatibvesNumbers problem

---
+++ CountOfPositive-SumOfNegatibvesNumbers/main.js
@@ -0,0 +1,20 @@
+
+
+function print(arr)
+{
+ let positivesArray=[];
+ let sumNegatives=0;
+  for (let index = 0; index < arr.length; index++) {
+    if(arr[index] > 0)
+    {
+        positivesArray.push(arr[index])
+    }
+    else
+    {
+        sumNegatives+=arr[index]
+    }
+  }
+  return [positivesArray,sumNegatives]
+}
+
+console.log(print([1,2,3,4,5,6,7,-5,-2]));
